7a66753196a13e728a1979bfd9c49f631f8810f8
Thai <tcao4@students.kennesaw.edu>
2019-04-29 17:45:49 -0400

    updating main.py
    added instructions

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #Bird Dodging Game
 #Coders: Thai Cao, Tran Huynh
 
-#Setup Game
+#Setup Game [Thai]
 import pygame, random, sys
 from pygame.locals import *
 
@@ -17,12 +17,41 @@ BIRDMAXSPEED = 10
 ADDNEWBIRDRATE = 3
 PLAYERMOVERATE = 25
 
-# set up pygame, the window, and the mouse cursor
+# set up pygame, the window, and the mouse cursor [Thai]
 
-# set up fonts
+# set up fonts [Tran]
 
-# set up sounds
+# set up sounds [Tran]
 
-# set up images
+# set up images [Thai]
 
-# show the "Start" screen
+# show the "Start" screen [Tran]
+
+# set up the start of the game [Tran]
+
+# the game loop runs while the game part is playing [Thai]
+
+# If the mouse moves, move the player where the cursor is. [Thai]
+
+# Add new birds at the top of the screen, if needed. [Thai]
+
+# Move the player around. [Tran]
+
+# Move the mouse cursor to match the player. [Thai]
+
+# Move the birds down. [Tran]
+
+
+# Delete birds that have fallen past the bottom. [Thai]
+
+# Draw the game world on the window. [Thai]
+
+# Draw the score and top score. [Tran]
+
+# Draw the player's rectangle [Tran]
+
+# Draw each bird [Thai]
+
+# Check if any of the birds have hit the player. [Tran]
+
+# Stop the game and show the "Game Over" screen. [Tran]
