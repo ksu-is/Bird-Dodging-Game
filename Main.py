281bdde264e4a056e2e1f9d0dd2393b1bd478759
Thai <tcao4@students.kennesaw.edu>
2019-04-29 16:42:14 -0400

    Updating codes
    added set up

--- Main.py
+++ Main.py
@@ -2,4 +2,20 @@
 #Coders: Thai Cao, Tran Huynh
 
 #Setup Game
-#Test
+
+import pygame, random, sys
+from pygame.locals import *
+
+
+
+WINDOWWIDTH = 1200
+WINDOWHEIGHT = 600
+TEXTCOLOR = (255, 255, 255)
+BACKGROUNDCOLOR = pygame.image.load('CloudBackground.png')
+FPS = 120
+BIRDMINSIZE = 40
+BIRDMAXSIZE = 50
+BIRDMINSPEED = 2
+BIRDMAXSPEED = 10
+ADDNEWBIRDRATE = 3
+PLAYERMOVERATE = 25
