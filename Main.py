2be7cb8c02cd1469be5b7a7b5d407c4632a9bdc6
thuynh35 <48929690+thuynh35@users.noreply.github.com>
2019-04-29 19:31:14 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -136,7 +136,17 @@ while True:
             pokis.append(newPoki)
 # Move the player around. [Tran]
 
- 
+  if moveLeft and playerRect.left > 0:
+            playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
+
+        if moveRight and playerRect.right < WINDOWWIDTH:
+            playerRect.move_ip(PLAYERMOVERATE, 0)
+
+        if moveUp and playerRect.top > 0:
+            playerRect.move_ip(0, -1 * PLAYERMOVERATE)
+
+        if moveDown and playerRect.bottom < WINDOWHEIGHT:
+            playerRect.move_ip(0, PLAYERMOVERATE)
 
 
 
