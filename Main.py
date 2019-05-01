96f14bae7e98c077fe688704708886580b28e268
Thai <tcao4@students.kennesaw.edu>
2019-04-30 20:33:06 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,6 +17,33 @@ BIRDMAXSPEED = 10
 ADDNEWBIRDRATE = 3
 PLAYERMOVERATE = 25
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def waitForPlayerToPressKey():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE: # pressing escape quits
+                    terminate()
+                return
+
+def playerHasHitBird(playerRect, birds):
+    for b in birds:
+        if playerRect.colliderect(b['rect']):
+            return True
+    return False
+
+def drawText(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
 # set up pygame, the window, and the mouse cursor [Thai]
 pygame.init()
 mainClock = pygame.time.Clock()
